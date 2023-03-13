560c3b422012de79bf1b7305e2cc1fb1017e1cf5
Wizat <passavit1@gmail.com>
2023-03-13 12:50:06 +0700

    commit

---
+++ npm.js
@@ -0,0 +1,10 @@
+var uc = require("upper-case");
+var http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write(uc.upperCase("hello world"));
+    res.end();
+  })
+  .listen(888);
