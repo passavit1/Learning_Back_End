f35112fa1ab86b006a6b5f8e61e06968a66c88f7
Wizat <passavit1@gmail.com>
2023-07-26 22:52:15 +0700

    learning escaping query

--- mySQL/index.js
+++ mySQL/index.js
@@ -7,17 +7,57 @@ var con = mysql.createConnection({
   database: "wizat",
 });
 
+// Escaping Query
+
+// con.connect((error) => {
+//   if (error) throw error;
+//   console.log("Connected");
+
+//   var adr = "cnx";
+//   var name = "passavit";
+
+// var sql = "SELECT * FROM customer where address = " + mysql.escape(adr);
+// con.query(sql, (err, result) => {
+//   if (err) throw err;
+//   console.log(result);
+// });
+
+//   var sql = "SELECT * FROM customer WHERE name = ? or address = ?";
+//   con.query(sql, [name, adr], (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
+
+// where
+
+// con.connect((error) => {
+//   if (error) throw error;
+//   console.log("Connected");
+
+//   con.query(
+//     "select * from customer where address like 'b%'",
+//     (error, result, feilds) => {
+//       if (error) throw error;
+//       console.log(result);
+//     }
+//   );
+// });
+
 // Select
 
-con.connect((err) => {
-  if (err) throw err;
-  console.log("Connected");
+// con.connect((err) => {
+//   if (err) throw err;
+//   console.log("Connected");
 
-  con.query("select name , address from customer", (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  });
-});
+//   con.query(
+//     "select id , name , address from customer",
+//     (err, result, fields) => {
+//       if (err) throw err;
+//       console.log(fields[0]);
+//     }
+//   );
+// });
 
 // Fill a table
 
