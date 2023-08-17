90d01fa3a8666d62ea9e39d53b80e1dbd712da22
Passavit_Gunn <passavit1@gmail.com>
2023-08-18 00:25:04 +0700

    learn jwt

--- api/practice.js
+++ api/practice.js
@@ -2,53 +2,70 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const config = process.env;
+const port = config.PORT;
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
-const config = process.env;
-const port = config.PORT || 3000;
-
-const user = [];
+const users = [];
 
 app.listen(port, () => {
-  console.log(`this server is listening on ${port}`);
+  console.log(`This port is listening on ${port}`);
 });
 
-app.post("/user/create", async (req, res) => {
-  let data = req.body;
+app.get("/", (req, res) => {
+  res.status(200).send("hello world");
+});
 
-  const checkUser = user.find((x) => x.username === data.username);
-  if (checkUser) return res.status(200).json({ message: "user duplicated" });
+app.post("/create", async (req, res) => {
+  const data = req.body;
 
-  const token = await jwt.sign({ username: data.username }, config.TOKEN_KEY, {
+  const encryptedPWD = await bcrypt.hash(data?.password, 10);
+  data.password = encryptedPWD;
+
+  const JWT_Token = jwt.sign(data, config.TOKEN_KEY, {
     expiresIn: "1h",
   });
 
-  const encryptedPWD = await bcrypt.hash(data.password, 10);
-
-  data.password = encryptedPWD;
-  data._token = token;
-  user.push(data);
-
-  console.log(data);
-
-  res.status(200).json({ message: "Create user successfully", token: token });
+  delete data?.password;
+  data.token = JWT_Token;
+  users.push(data);
+  res.status(200).json({ message: "create successfully" });
 });
 
-app.get("/user/data", (req, res) => {
-  res.status(200).send(user);
+app.get("/data", (req, res) => {
+  res.status(200).send(users);
 });
 
-app.put("/user/edit/:username", (req, res) => {
-  const pointer = req.params.username;
-  const newData = req.body;
+const verifyToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+  const loginUser = req.body;
 
-  const checkUser = user.find((x) => x.username === pointer);
+  if (!token) {
+    return res.status(403).json({ message: "Token missing" });
+  }
 
-  if (!checkUser) return res.status(401).json({ message: "user not found" });
+  try {
+    const checkUser = users.find((x) => x.username === loginUser.username);
 
-  Object.assign(checkUser, newData);
-  res.status(200).json({ message: "Update new data successful" });
+    if (!checkUser) return res.status(403).json({ message: "User not found" });
+
+    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    const checkPWD = await bcrypt.compare(loginUser.password, decoded.password);
+
+    if (checkPWD) {
+      req.user = decoded;
+      next();
+    } else {
+      res.status(401).json({ error: "password or username wrong" });
+    }
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+app.get("/protected", verifyToken, (req, res) => {
+  res.json({ message: "Access granted", user: req.user });
 });
